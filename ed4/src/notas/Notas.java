f46a08a758d3393111a7067a5bf286f619abedbf
miguelpb97 <57225564+miguelpb97@users.noreply.github.com>
2023-03-03 01:42:03 +0100

    ejercicio6

--- ed4/src/notas/Notas.java
+++ ed4/src/notas/Notas.java
@@ -1,40 +1,23 @@
 package notas;
 
-import java.util.Scanner;
-
 public class Notas {
 	public static void main(String[] args) {
-		califica();
+		notas.califica.califica();
 	}
 
 	/**
-	 * @deprecated Use {@link #califica()} instead
+	 * @deprecated Use {@link notas.califica#califica()} instead
 	 * 
 	 */
 	public static void cali() {
-		califica();
+		notas.califica.califica();
 	}
 
 	/**
+	 * @deprecated Use {@link califica#califica()} instead
 	 * 
 	 */
 	public static void califica() {
-		Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-		int nota = 0;
-		String calificacion = "";
-		System.out.print("Introduzca el valor de una calificación: ");
-		nota = reader.nextInt();
-		if (nota >= 0 && nota < 5)
-			calificacion = "Suspenso";
-		else if (nota >= 5 && nota < 7)
-			calificacion = "Aprobado";
-		else if (nota >= 7 && nota < 9)
-			calificacion = "Notable";
-		else if (nota >= 9 && nota <= 10)
-			calificacion = "Sobresaliente";
-		else
-			calificacion = "El valor de la calificación introducida no es correcta";
-		System.out.println(calificacion);
-		reader.close();
+		califica.califica();
 	}
 }
